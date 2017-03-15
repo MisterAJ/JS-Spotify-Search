0e65c8b99be99535d4e6ceb549a29e83931e07e9
AJ Longstreet <ajlongstreet@gmail.com>
2017-03-14 23:47:14 -0400

    fixed html and added message for No Result

--- js/app.js
+++ js/app.js
@@ -16,15 +16,15 @@ $('form').submit(function (evt) {
     let displayAlbumHTML = '';
     function displayAlbum(spotifyResponse) {
         $('.desc').remove();
-        $.each(spotifyResponse.albums.items, function (i,item) {
+        $.each(spotifyResponse.albums.items, function (i,album) {
             displayAlbumHTML += `
 	    			<li>
-	    				<a href="${item.external_urls.spotify}" target="_blank" id=${i}>
+	    				<a href="${album.external_urls.spotify}" target="_blank" id=${i}>
 	    					<div class="album-wrap">
-	    						<img class="album-art" src="${item.images[0].url}">
+	    						<img class="album-art" src="${album.images[0].url}">
 	    					</div>
-		    				<span class="album-title">${item.name}</span>
-		    				<span class="album-artist">${item.artists[0].name}</span>
+		    				<span class="album-title">${album.name}</span>
+		    				<span class="album-artist">${album.artists[0].name}</span>
 	    				</a>
 	    			</li>
     			`;
@@ -32,8 +32,18 @@ $('form').submit(function (evt) {
 
         $searchField.prop("disabled", false);
         $submitButton.attr("disabled", false);
+
+        if(displayAlbumHTML === '') {
+            displayAlbumHTML = `
+            <li class='no-albums'>
+                <i class='material-icons icon-help'>help_outline</i>No albums found that match: [search form value].
+            </li>`
+        }
+
         $('#albums').html(displayAlbumHTML);
+
     }
+
 
     $.getJSON(spotifyAPI,data,displayAlbum);
 });
