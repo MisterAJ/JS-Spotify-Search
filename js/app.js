c89313f240d0fd53ac21c3f49c73156a99306fe2
AJ Longstreet <ajlongstreet@gmail.com>
2017-03-14 22:14:42 -0400

    First functional commit

--- js/app.js
+++ js/app.js
@@ -8,15 +8,31 @@ $('form').submit(function (evt) {
 
 
     let spotifyAPI = "https://api.spotify.com/v1/search";
-    let searchTerm = encodeURI($search.val());
-    let data = {q: searchTerm};
-    function displayAlbum() {
+    let searchTerm = encodeURI($searchField.val());
+    let data = {q: searchTerm,
+                type: "album"};
+    console.log(data);
 
-
-
+    let displayAlbumHTML = '';
+    function displayAlbum(spotifyResponse) {
+        $('.desc').remove();
+        $.each(spotifyResponse.albums.items, function (i,item) {
+            displayAlbumHTML += `
+	    			<li>
+	    				<a href="${item.external_urls.spotify}" id=${i}>
+	    					<div class="album-wrap">
+	    						<img class="album-art" src="${item.images[0].url}">
+	    					</div>
+		    				<span class="album-title">${item.name}</span>
+		    				<span class="album-artist">${item.artists[0].name}</span>
+	    				</a>
+	    			</li>
+    			`;
+        });
 
         $searchField.prop("disabled", false);
         $submitButton.attr("disabled", false);
+        $('#albums').html(displayAlbumHTML);
     }
 
     $.getJSON(spotifyAPI,data,displayAlbum);
